d28e34c7cbe0476526754fa934f9f203e9467f31
xxwtbad <69973807+xxwtbad@users.noreply.github.com>
2026-07-01 22:46:45 +0800

    Update Navbar.tsx

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -150,13 +150,7 @@ export default function Navbar() {
         }
       }
       animate();
-   // HTML 特殊字符转义函数
-  const escapeHtml = (str) => {
-  const div = document.createElement('div');
-  div.textContent = str;
-  return div.innerHTML;
-};;
-};
+
       // 彩蛋提示文字，等彩纸下完后出现
       setTimeout(() => {
         const msg = document.createElement("div");
