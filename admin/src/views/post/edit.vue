82df49d34e4308d4e24d38525b6e37cfe1c91483
Eumigo <69973807+xxwtbad@users.noreply.github.com>
2026-07-12 16:03:12 +0800

    Update edit.vue

--- admin/src/views/post/edit.vue
+++ admin/src/views/post/edit.vue
@@ -321,10 +321,19 @@ onMounted(async () => {
 
         <el-form-item label="正文">
           <div class="w-full">
-            <Vditor
-              v-model="form.content"
-              :options="{ height: 500 }"
-            />
+        <Vditor
+            v-model="form.content"
+              :options="{
+               height: 500,
+               upload: {
+               url: "/api/upload/image",
+            fieldName: "file",
+            headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`
+          }
+          }
+          }"
+      />
           </div>
         </el-form-item>
       </el-form>
